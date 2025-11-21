e17e0ab43d797b22df86c77550111ca7473ec2c2
Thars2608 <143402345+Thars2608@users.noreply.github.com>
2025-11-21 14:28:18 +0100

    Revert "Authenthication folder React"
    This reverts commit 5aa2e838f98ad61e572813a13ddad79667d46a73.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,162 +1,56 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import "./App.css";
-import NavMenu from "./shared/NavMenu";
-import HomePage from "./Home/HomePage";
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import './App.css'
+import NavMenu from './shared/NavMenu'
+import HomePage from './Home/HomePage'
 // Available Days
-import AvailableDaysListPage from "./AvailableDays/AvailableDayListPage";
-import AvailableDayCreatePage from "./AvailableDays/AvailableDayCreatePage";
-import AvailableDayUpdatePage from "./AvailableDays/AvailableDayUpdatePage";
-
+import AvailableDaysListPage from './AvailableDays/AvailableDayListPage'
+import AvailableDayCreatePage from './AvailableDays/AvailableDayCreatePage'
+import AvailableDayUpdatePage from './AvailableDays/AvailableDayUpdatePage'
 // Appointment
-import AppointmentListPage from "./Appointment/AppointmentListPage";
-import AppointmentCreatePage from "./Appointment/AppointmentCreatePage";
-import AppointmentUpdatePage from "./Appointment/AppointmentUpdatePage";
-
+import AppointmentListPage from './Appointment/AppointmentListPage'
+import AppointmentCreatePage from './Appointment/AppointmentCreatePage'
+import AppointmentUpdatePage from './Appointment/AppointmentUpdatePage'
 // Patients
-import PatientListPage from "./Patient/PatientListPage";
-import PatientCreatePage from "./Patient/PatientCreatePage";
-import PatientUpdatePage from "./Patient/PatientUpdatePage";
-
+import PatientListPage from './Patient/PatientListPage'
+import PatientCreatePage from './Patient/PatientCreatePage'
+import PatientUpdatePage from './Patient/PatientUpdatePage'
 // Personnel
-import PersonnelListPage from "./Personnel/PersonnelListPage";
-import PersonnelCreatePage from "./Personnel/PersonnelCreatePage";
-import PersonnelUpdatePage from "./Personnel/PersonnelUpdatePage";
+import PersonnelListPage from './Personnel/PersonnelListPage'
+import PersonnelCreatePage from './Personnel/PersonnelCreatePage'
+import PersonnelUpdatePage from './Personnel/PersonnelUpdatePage'
 
-// Auth
-import { AuthProvider } from "./Auth/AuthContext";
-import ProtectedRoute from "./Auth/ProtectedRoute";
-import LoginPage from "./Auth/LoginPage";
-import RegisterPage from "./Auth/RegisterPage";
-
-const App: React.FC = () => {
-  return (
-    <AuthProvider>
-      <Router>
-        <NavMenu />
-        <Container className="mt-3">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-
-            {/* Auth */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-
-            {/* Available Days pages */}
-            <Route
-              path="/availabledays"
-              element={
-                <ProtectedRoute>
-                  <AvailableDaysListPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/availabledayscreate"
-              element={
-                <ProtectedRoute>
-                  <AvailableDayCreatePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/availabledaysupdate"
-              element={
-                <ProtectedRoute>
-                  <AvailableDayUpdatePage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Appointment pages */}
-            <Route
-              path="/appointment"
-              element={
-                <ProtectedRoute>
-                  <AppointmentListPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/appointmentcreate"
-              element={
-                <ProtectedRoute>
-                  <AppointmentCreatePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/appointmentupdate"
-              element={
-                <ProtectedRoute>
-                  <AppointmentUpdatePage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Patient pages */}
-            <Route
-              path="/patients"
-              element={
-                <ProtectedRoute>
-                  <PatientListPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/patientcreate"
-              element={
-                <ProtectedRoute>
-                  <PatientCreatePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/patientupdate/:patientId"
-              element={
-                <ProtectedRoute>
-                  <PatientUpdatePage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Personnel pages */}
-            <Route
-              path="/personnels"
-              element={
-                <ProtectedRoute>
-                  <PersonnelListPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/personnelcreate"
-              element={
-                <ProtectedRoute>
-                  <PersonnelCreatePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/personnelupdate/:personnelId"
-              element={
-                <ProtectedRoute>
-                  <PersonnelUpdatePage />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </Container>
-      </Router>
-    </AuthProvider>
-  );
-};
-
-export default App;
-
+const App: React.FC=() => {
+  return(
+    <Router>
+    <NavMenu />
+    <Container className="mt-3">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+                    {/* Available Days pages */}
+          <Route path="/availabledays" element={<AvailableDaysListPage />} />
+          <Route path="/availabledayscreate" element={<AvailableDayCreatePage />} />
+          <Route path="/availabledaysupdate" element={<AvailableDayUpdatePage />} />
+                    {/* Appointment pages */}
+          <Route path="/appointment" element={<AppointmentListPage />} />
+          <Route path="/appointmentcreate" element={<AppointmentCreatePage />} />
+          <Route path="/appointmentupdate" element={<AppointmentUpdatePage />} />
+                    {/* Patient pages */}
+          <Route path="/patients" element={<PatientListPage />} />
+          <Route path="/patientcreate" element={<PatientCreatePage />} />
+          <Route path="/patientupdate/:patientId" element={<PatientUpdatePage />} />
+                    {/* Personnel pages */}   
+          <Route path="/personnels" element={<PersonnelListPage />} />
+          <Route path="/personnelcreate" element={<PersonnelCreatePage />} />
+          <Route path="/personnelupdate/:personnelId" element={<PersonnelUpdatePage />} />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    </Container>
+</Router>
+  )
+}
+export default App
 /*function App() {
   const [count, setCount] = useState(0)
 
--- frontend/src/types/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  sub: string;      // brukernavn
-  nameid: string;   // userId i databasen 
-  jti: string;      // unik id for tokenet
-  iat: number;      // issued at (seconds since epoch)
-  exp: number;      // expires at (seconds since epoch)
-  iss: string;      // issuer
-  aud: string;      // audience
-}
