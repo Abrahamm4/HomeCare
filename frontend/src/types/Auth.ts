25799f9af01fabde1daf8f6d2acd00b85a187cee
Thars2608 <143402345+Thars2608@users.noreply.github.com>
2025-11-21 14:44:52 +0100

    Same as the last commit
    Same as the last commit before the revert changes. This time database file is not included

---
+++ frontend/src/types/Auth.ts
@@ -0,0 +1,11 @@
+export interface LoginDto {
+  username: string;
+  password: string;
+}
+
+export interface RegisterDto {
+  username: string;
+  email: string;
+  password: string;
+  //Can add more fields if needed
+}
